5684840b3bb41eca97474c9f2eb24665f04bb706
MartijnvCitteren <141489106+MartijnvCitteren@users.noreply.github.com>
2024-01-09 14:49:45 +0100

    Updated header and footer  + created index layout

--- src/main/java/com/martijn/CompCheck/BenefitPackage/BenefitPackage.java
+++ src/main/java/com/martijn/CompCheck/BenefitPackage/BenefitPackage.java
@@ -1,27 +1,145 @@
 package com.martijn.CompCheck.BenefitPackage;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name= "benefitpackage", schema = "public")
 public class BenefitPackage {
     @Id
     @GeneratedValue
+    @Column(name = "id")
     private Integer id;
-    private String company;
-    private String cao;
-    private Double salary;
-    private String salaryPeriod;
-    private Integer hoursPerPeriod;
+
+    @Column(name = "salary")
+    private Double salaryMonthly;
+
+    @Column(name = "work_hours_weekly")
+    private Integer hoursPerWeek;
+
+    @Column(name = "fixed_bonus_yearly")
     private Double fixedYearlyBonus;
+
+    @Column(name = "pto_hours_fixed_yearly")
     private Integer obligatoryPtoHoursPerYear;
+
+    @Column(name = "pto_hours_sellable_yearly")
     private Integer sellablePtoHoursPerYear;
+
+    @Column(name = "pension_employer_contribution")
     private Double employerPensionContribution;
+
+    @Column(name = "pension_employee_contribution")
     private Double applicantPensionContribution;
-    private Double nettSalaryYearly;
+
+    @Column(name = "gross_salary_yearly")
+    private Double grossSalaryYearly;
+
+    @Column(name = "caoid")
+    private Integer caoId;
+
+    @Column(name = "companyid")
+    private Integer companyId;
+
+    @Column(name = "userid")
+    private Integer appUserId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getSalaryMonthly() {
+        return salaryMonthly;
+    }
+
+    public void setSalaryMonthly(Double salaryMonthly) {
+        this.salaryMonthly = salaryMonthly;
+    }
+
+    public Integer getHoursPerWeek() {
+        return hoursPerWeek;
+    }
+
+    public void setHoursPerWeek(Integer hoursPerWeek) {
+        this.hoursPerWeek = hoursPerWeek;
+    }
+
+    public Double getFixedYearlyBonus() {
+        return fixedYearlyBonus;
+    }
+
+    public void setFixedYearlyBonus(Double fixedYearlyBonus) {
+        this.fixedYearlyBonus = fixedYearlyBonus;
+    }
+
+    public Integer getObligatoryPtoHoursPerYear() {
+        return obligatoryPtoHoursPerYear;
+    }
+
+    public void setObligatoryPtoHoursPerYear(Integer obligatoryPtoHoursPerYear) {
+        this.obligatoryPtoHoursPerYear = obligatoryPtoHoursPerYear;
+    }
+
+    public Integer getSellablePtoHoursPerYear() {
+        return sellablePtoHoursPerYear;
+    }
+
+    public void setSellablePtoHoursPerYear(Integer sellablePtoHoursPerYear) {
+        this.sellablePtoHoursPerYear = sellablePtoHoursPerYear;
+    }
+
+    public Double getEmployerPensionContribution() {
+        return employerPensionContribution;
+    }
+
+    public void setEmployerPensionContribution(Double employerPensionContribution) {
+        this.employerPensionContribution = employerPensionContribution;
+    }
+
+    public Double getApplicantPensionContribution() {
+        return applicantPensionContribution;
+    }
+
+    public void setApplicantPensionContribution(Double applicantPensionContribution) {
+        this.applicantPensionContribution = applicantPensionContribution;
+    }
+
+    public Double getGrossSalaryYearly() {
+        return grossSalaryYearly;
+    }
+
+    public void setGrossSalaryYearly(Double grossSalaryYearly) {
+        this.grossSalaryYearly = grossSalaryYearly;
+    }
+
+    public Integer getCaoId() {
+        return caoId;
+    }
+
+    public void setCaoId(Integer caoId) {
+        this.caoId = caoId;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public Integer getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(Integer appUserId) {
+        this.appUserId = appUserId;
+    }
 }
--- src/main/java/com/martijn/CompCheck/BenefitPackage/BenefitPackageController.java
+++ src/main/java/com/martijn/CompCheck/BenefitPackage/BenefitPackageController.java
@@ -1,12 +1,14 @@
 package com.martijn.CompCheck.BenefitPackage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path ="/benefit-package")
+@Controller
+@RequestMapping
 public class BenefitPackageController {
 
     private final BenefitPackageService benefitPackageService;
@@ -15,6 +17,12 @@ public class BenefitPackageController {
     public  BenefitPackageController(BenefitPackageService benefitPackageService) {
         this.benefitPackageService = benefitPackageService;
     }
+
+    @GetMapping("/index")
+    public String benfitPackage(Model model) {
+        return "index";
+    }
+
 
 
 
